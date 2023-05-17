216cce1c0fe40ec36fbeb8b45113d9ec4e76ee54
JulianLopez93 <andersson.1701522324@ucaldas.edu.com>
2023-05-17 15:10:38 -0500

    Se hace funcion sumar

--- funciones.js
+++ funciones.js
@@ -10,3 +10,15 @@ function multiplicarNumeros(req, res) {
   }
 }
 
+function sumarNumeros(req, res) {
+  const { numero1, numero2 } = req.query; // obtenemos los números de la query string
+  
+  // comprobamos si los parámetros son números
+  if (isNaN(numero1) || isNaN(numero2)) {
+    res.status(400).send('Los parámetros deben ser números.');
+  } else {
+    const resultado = parseFloat(numero1) + parseFloat(numero2); // multiplicamos los números
+    res.send(`El resultado de la multiplicación es: ${resultado}`);
+  }
+}
+
